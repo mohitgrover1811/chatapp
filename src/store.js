856577a4ad0833faa3b3b2636d01d88a368d6b0c
mohitgrover1811 <109900687+mohitgrover1811@users.noreply.github.com>
2022-07-24 19:09:44 +0800

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { legacy_createStore as createStore } from "redux";
-
 import rootReducer from "./reducers/rootReducer";
+import { loadState, saveState } from './localStorage';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(rootReducer, loadState());
+
+store.subscribe(() => saveState(store.getState()));
 
 export default store;
